063d0b1810f39706f0800f799da89e2dc6062bb9
Molkie <55138192+Molkie@users.noreply.github.com>
2019-10-08 13:16:09 +0200

    Första lektionen
    Jobbat lite

---
+++ FightSim/FightSim/Fighter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightSim
+{
+    class Fighter
+    {
+        int hp;
+        public string name;
+
+        public int Attack()
+        {
+            int dmg = 5;
+            return (dmg);
+        }
+
+        public void Hurt(int amount)
+        {
+            hp -= amount;
+        }
+
+        public bool isAlive()
+        {
+            bool alive;
+            if(hp > 0)
+            {
+                alive = true;
+            }
+            else
+            {
+                alive = false;
+            }
+            return alive;
+        }
+
+        public int GetHp()
+        {
+            return (hp);
+        }
+
+    }
+}
---
+++ FightSim/FightSim/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array som samlar de båda fighters.
+
+            Console.WriteLine("Hello planet");
+            //Kallar metoden CreateFighter 2 gånger, en för varge fighter. Lagrar resultatet i instanserna fighter 1 och 2.
+            Fighter fighter1 = CreateFighter();
+            Fighter fighter2 = CreateFighter();
+            //Skapar en array att spara dessa fighters i.
+            Fighter[] roster = { fighter1, fighter2 };
+
+
+
+            Console.ReadLine();
+        }
+
+
+        //Metod som skapar en fighter
+        static Fighter CreateFighter()
+        {
+            //Skapar en ny fighter
+            Fighter fighter = new Fighter();
+            //Instruktioner till användaren
+            Console.WriteLine("Please type a name for the fighter: ");
+            fighter.name = Console.ReadLine().Trim();
+            return fighter;
+        }
+    }
+}
